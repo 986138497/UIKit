47bf95ede7f486becc357cb6094b67a2d2d827c2
986138497 <986138497@qq.com>
2016-11-21 14:15:01 +0800

    Initial Commit

---
+++ UIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKit
+//
+//  Created by lei on 2016/11/21.
+//  Copyright © 2016年 lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIKit
+//
+//  Created by lei on 2016/11/21.
+//  Copyright © 2016年 lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
